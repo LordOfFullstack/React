472369bd4d8cbe0aa5c4f946fee25434bf466226
Вячеслав Флорчук <FlorchukVV@office.ukroil.com.ua>
2017-12-12 19:15:11 +0200

    fix webpack config

--- todolist/webpack.config.js
+++ todolist/webpack.config.js
@@ -1,39 +1,38 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var DIST_DIR = path.resolve(__dirname, 'dist');
 var SRC_DIR = path.resolve(__dirname, './src');
 
 module.exports = {
-    entry: SRC_DIR + '/app/index.js',
-    output: {
-        path: DIST_DIR + '/app',
-        filename: 'bundle.js',
-        publicPath: '/app/',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: SRC_DIR,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react', 'es2015', 'stage-2']
-                }
-            },
-            {
-                test: /\.html$/,
-                include: SRC_DIR,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css/,
-                include: SRC_DIR,
-                loader: 'style-loader!css-loader!autoprefixer-loader!less-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-};
+  entry: SRC_DIR + '/app/index.js',
+  output: {
+    path: DIST_DIR + '/app',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      },
+      {
+        test: /\.css/,
+        include: SRC_DIR,
+        loader: 'style-loader!css-loader!autoprefixer-loader!less-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
