8673e146f0f9442f1a6e31b06ea19ed48286d680
Вячеслав Флорчук <FlorchukVV@office.ukroil.com.ua>
2018-01-19 18:09:28 +0200

    react-router-messages

---
+++ react-router-messages/src/components/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <div className='AboutPage'>
+        <p>AboutPage</p>
+      </div>
+    )
+  }
+}
+
+export default AboutPage;
---
+++ react-router-messages/src/components/InboxPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InboxPage extends Component {
+  render() {
+    return (
+      <div className='InboxPage'>
+        <p>InboxPage</p>
+      </div>
+    )
+  }
+}
+
+export default InboxPage;
